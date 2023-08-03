8ce533cf8500a2c88062811f10a2a13711f3b29d
George Raafat <114592858+5XGeorgeX5@users.noreply.github.com>
2023-08-03 17:41:53 +0300

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -5,32 +5,25 @@
  * @it1: the fist it
  * @it2: the second it
  * @list: the list
- * @head: the head
- * @tail: the tail
  *
  * Return: 1 or 0
  */
-int swap_list(listint_t *it1, listint_t *it2, listint_t **list,
-                            listint_t **head, listint_t **tail)
+int swap_list(listint_t *it1, listint_t *it2, listint_t **list)
 {
-    if (it1->n <= it2->n)
-        return (0);
-    if (it2->next)
-        it2->next->prev = it1;
-    if (it1->prev)
-        it1->prev->next = it2;
-    else
-        *list = it2;
-    if (it2 == *tail)
-        *tail = it1;
-    if (it1 == *head)
-        *head = it2;
-    it1->next = it2->next;
-    it2->next = it1;
-    it2->prev = it1->prev;
-    it1->prev = it2;
-    print_list(*list);
-    return (1);
+	if (it1->n <= it2->n)
+		return (0);
+	if (it2->next)
+		it2->next->prev = it1;
+	if (it1->prev)
+		it1->prev->next = it2;
+	else
+		*list = it2;
+	it1->next = it2->next;
+	it2->next = it1;
+	it2->prev = it1->prev;
+	it1->prev = it2;
+	print_list(*list);
+	return (1);
 }
 
 /**
@@ -39,30 +32,27 @@ int swap_list(listint_t *it1, listint_t *it2, listint_t **list,
  */
 void cocktail_sort_list(listint_t **list)
 {
-    int swapped = 1;
-    listint_t *head = *list, *tail = *list, *it;
+	int swapped = 1;
+	listint_t *tail, *it;
 
-    if (!list || !*list || !(*list)->next)
-        return;
-    while (tail->next)
-        tail = tail->next;
-    while(swapped)
-    {
-        swapped = 0;
-        for (it = head->next; it != tail->next; it = it->next)
-        {
-            if (swap_list(it->prev, it, list, &head, &tail))
-                swapped = 1;
-        }
-        if (!swapped)
-            break;
-        swapped = 0;
-        tail = tail->prev;
-        for (it = tail->prev; it != head->prev; it = it->prev)
-        {
-            if (swap_list(it, it->next, list, &head, &tail))
-                swapped = 1;
-        }
-        head = head->next;
-    }
+	if (!list || !*list || !(*list)->next)
+		return;
+	while (swapped)
+	{
+		swapped = 0;
+		for (it = (*list)->next; it; it = it->next)
+		{
+			if (swap_list(it->prev, it, list))
+				swapped = 1;
+			tail = it;
+		}
+		if (!swapped)
+			break;
+		swapped = 0;
+		for (it = tail->prev; it; it = it->prev)
+		{
+			if (swap_list(it, it->next, list))
+				swapped = 1;
+		}
+	}
 }
